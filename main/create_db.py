d0c2dcfd85652bfc00513e812d041be5fb0babaa
rvinw <rviinw@gmail.com>
2025-04-26 15:13:10 +0100

    smth

--- main/create_db.py
+++ main/create_db.py
@@ -7,13 +7,86 @@ cur.execute('''
 CREATE TABLE IF NOT EXISTS staff (
     userID INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
-    userForename TEXT NOT NULL UNIQUE,
-    userSurname TEXT NOT NULL UNIQUE,
-    userType INTEGER NOT NULL CHECK(usertype IN (1, 2, 3)),
+    userForename TEXT NOT NULL,
+    userSurname TEXT NOT NULL,
+    userType INTEGER NOT NULL CHECK(userType IN (1, 2, 3)),
     userPassword TEXT NOT NULL
 )
 ''')
 
+cur.execute('''
+CREATE TABLE IF NOT EXISTS movie (
+    movieID INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE,
+    description TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    age TEXT NOT NULL,
+    rating FLOAT NOT NULL,
+    cast TEXT NOT NULL
+)
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS city (
+    cityID INTEGER PRIMARY KEY AUTOINCREMENT,
+    cityName TEXT NOT NULL UNIQUE,
+    basePrice INTEGER NOT NULL
+)
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS cinema (
+    cinemaID INTEGER PRIMARY KEY AUTOINCREMENT,
+    cinemaName TEXT NOT NULL,
+    cityID INTEGER NOT NULL,
+    FOREIGN KEY (cityID) REFERENCES city(cityID)
+)
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS screen (
+    screenID INTEGER PRIMARY KEY AUTOINCREMENT,
+    cinemaID INTEGER NOT NULL UNIQUE,
+    capacity INTEGER NOT NULL,
+    FOREIGN KEY (cinemaID) REFERENCES cinema(cinemaID)
+)
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS show (
+    showID INTEGER PRIMARY KEY AUTOINCREMENT,
+    movieID INTEGER NOT NULL,
+    showTime TEXT NOT NULL,
+    screenID INTEGER NOT NULL,
+    FOREIGN KEY (movieID) REFERENCES movie(movieID),
+    FOREIGN KEY (screenID) REFERENCES screen(screenID)
+)
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS seat (
+    seatID INTEGER NOT NULL,
+    seatType TEXT NOT NULL,
+    screenID INTEGER NOT NULL,
+    PRIMARY KEY (seatID, screenID),
+    FOREIGN KEY (screenID) REFERENCES screen(screenID)
+)
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS booking (
+    bookingID INTEGER NOT NULL,
+    showID INTEGER NOT NULL,
+    seatID INTEGER NOT NULL,
+    price FLOAT NOT NULL,
+    cancelled BOOLEAN DEFAULT 0,
+    PRIMARY KEY (bookingID, seatID),
+    FOREIGN KEY (showID) REFERENCES show(showID),
+    FOREIGN KEY (seatID) REFERENCES seat(seatID)
+)
+''')
+
+#Example insert
 cur.execute('''
 INSERT INTO staff (username, userForename, userSurname, userType, userPassword)
 VALUES (?, ?, ?, ?, ?)
