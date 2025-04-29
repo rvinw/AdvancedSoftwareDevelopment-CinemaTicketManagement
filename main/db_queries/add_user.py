ccbfb8845a906e0f45ae2b24a0164b67cad83461
rvinw <rviinw@gmail.com>
2025-04-29 15:16:41 +0100

    added some queries

--- main/db_queries/add_user.py
+++ main/db_queries/add_user.py
@@ -4,13 +4,27 @@ def add_user(username, password, forename, surname, usertype):
     con = sqlite3.connect("HorizonCinema.db")
     cur = con.cursor()
 
-    cur.execute('''
-    INSERT INTO movie (username, userPassword, userForename, userSurname, userType)
-    VALUES (?, ?, ?, ?, ?)
-    ''', (username, password, forename, surname, usertype))
+    #cur.execute('''
+    #INSERT INTO movie (username, userPassword, userForename, userSurname, userType)
+    #VALUES (?, ?, ?, ?, ?)
+    #''', (username, password, forename, surname, usertype))
+    
+    conn = sqlite3.connect('HorizonCinema.db')
+    cur = conn.cursor()
+    cur.executemany('''
+    INSERT INTO staff (username, userForename, userSurname, userType, userPassword)
+        VALUES (?, ?, ?, ?, ?)
+    ''', [
+        ('BookingStaff', 'Vincent', 'Richardson-Price', 1, 'staffpass123'),
+        ('Manager', 'Jake', 'Richardson-Price', 2, 'manpass123'),
+        ('Admin', 'Alex', 'Nakhle', 3, 'adminpass123')
+    ])
 
-    con.commit()
-    con.close()
+    conn.commit()  # This actually saves the changes
+    conn.close()
+
+    #con.commit()
+    #con.close()
     
 username = ''
 password = ''
@@ -19,3 +33,4 @@ surname = ''
 usertype = ''
 
 add_user(username, password, forename, surname, usertype)
+
